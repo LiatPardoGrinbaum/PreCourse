b4d8c1d3a051d71f0f495ded0d155cdd94f95ae3
LiatPardoGrinbaum <pardo.liat@gmail.com>
2022-03-22 17:44:48 +0200

    added isTen function

---
+++ isTen.js
@@ -0,0 +1,11 @@
+const isTen = (a,b)=>{ 
+    //Your Code Here :-)
+        let answer = "";
+        if ((a+b) === 10) {
+            answer = "makes 10";
+            return answer;
+          } else {
+              answer = "nope";
+          }
+        return answer;
+    }
