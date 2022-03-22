3da846fae3bdb1355e6cd71bf07acfb610d1a48e
LiatPardoGrinbaum <pardo.liat@gmail.com>
2022-03-22 22:35:59 +0200

    Update isTen.js

--- isTen.js
+++ isTen.js
@@ -1,11 +1,10 @@
 const isTen = (a,b)=>{ 
-    //Your Code Here :-)
-        let answer = "";
-        if ((a+b) === 10) {
-            answer = "makes 10";
-            return answer;
-          } else {
-              answer = "nope";
-          }
+    let answer = "";
+    if ((a+b) === 10) {
+        answer = "makes 10";
         return answer;
-    }
+        } else {
+            answer = "nope";
+        }
+    return answer;
+}
